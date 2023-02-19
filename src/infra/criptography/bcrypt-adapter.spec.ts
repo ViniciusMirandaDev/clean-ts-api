5e72aaaf0e0ec987721fae0c5b6b28011a7c503f
ViniciusMirandaDev <vinimb07@gmail.com>
2023-02-19 14:06:19 -0300

    feat: ensure BCrypt Adapter throws if BCrypt throw

--- src/infra/criptography/bcrypt-adapter.spec.ts
+++ src/infra/criptography/bcrypt-adapter.spec.ts
@@ -24,4 +24,15 @@ describe('BCrypt Adapter', () => {
     const hash = await sut.encrypt('any_value')
     expect(hash).toBe('hash')
   })
+
+  test('Should throw if BCrypt throws', async () => {
+    const sut = makeSut()
+    jest.spyOn(bcrypt, 'hash').mockImplementationOnce(
+      () => {
+        throw new Error()
+      }
+    )
+    const promise = sut.encrypt('any_value')
+    await expect(promise).rejects.toThrow()
+  })
 })

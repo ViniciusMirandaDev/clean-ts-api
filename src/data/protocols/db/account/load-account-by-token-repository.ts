6c8fb0f8bbdc7005db90ab30dca4e09243109744
ViniciusMirandaDev <vinimb07@gmail.com>
2023-03-26 01:42:32 -0300

    feat: make result nullable

--- src/data/protocols/db/account/load-account-by-token-repository.ts
+++ src/data/protocols/db/account/load-account-by-token-repository.ts
@@ -1,5 +1,5 @@
 import { AccountModel } from '../../../usecases/add-account/db-add-account-protocols'
 
 export interface LoadAccountByTokenRepository {
-  loadByToken (email: string, role?: string): Promise<AccountModel>
+  loadByToken (email: string, role?: string): Promise<AccountModel | null>
 }

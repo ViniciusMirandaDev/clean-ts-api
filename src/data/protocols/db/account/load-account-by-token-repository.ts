1bf4c511a7886125680df2672e993e3f700faf66
ViniciusMirandaDev <vinimb07@gmail.com>
2023-03-26 02:22:42 -0300

    feat: ensure AccountMongoRepository returns an account on loadByToken without role

--- src/data/protocols/db/account/load-account-by-token-repository.ts
+++ src/data/protocols/db/account/load-account-by-token-repository.ts
@@ -1,5 +1,5 @@
 import { AccountModel } from '../../../usecases/add-account/db-add-account-protocols'
 
 export interface LoadAccountByTokenRepository {
-  loadByToken (email: string, role?: string): Promise<AccountModel | null>
+  loadByToken (token: string, role?: string): Promise<AccountModel | null>
 }
